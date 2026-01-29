b5e7f6e69738f666b9ffa1c341c8c17f33e6e072
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-01-29 09:48:59 -0300

    add controller and route to users endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import "express-async-errors";
-
+import { routes } from "@/routes/index.js";
 import { errorHandling } from "@/middlewares/error-handling.js";
 
 const app = express();
 
 app.use(express.json());
-
+app.use(routes);
 app.use(errorHandling);
 
 export { app };
