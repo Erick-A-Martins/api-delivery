771e74c694e35ef23a05c8e5a5b68d93db3858df
Erick-A-Martins <erickalvesqmartins@gmail.com>
2026-01-31 16:30:27 -0300

    delete the teste user afterAll testes

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,7 +1,14 @@
 import request from "supertest";
+import { prisma } from "@/database/prisma.js";
 import { app } from "@/app.js";
 
 describe("UsersController", () => {
+    let user_id: string
+
+    afterAll(async () => {
+        await prisma.user.delete({ where: { id: user_id } });
+    });
+
     it("should create a new user successfully", async () => {
         const response = await request(app).post("/users").send({
             name: "Test User",
@@ -12,5 +19,7 @@ describe("UsersController", () => {
         expect(response.status).toBe(201);
         expect(response.body).toHaveProperty("id");
         expect(response.body.name).toBe("Test User");
+
+        user_id = response.body.id;
     })
 })
